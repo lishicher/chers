73108896b0d6903b1a76b8e6c5826657c241c9df
lishichang <465655823@qq.com>
2019-07-06 11:35:53 +0800

    one

---
+++ app/src/main/java/bawei/com/lishichang201976/Bean.java
@@ -0,0 +1,9 @@
+package bawei.com.lishichang201976;
+
+/*
+ *@Auther:荒
+ *@Date: 2019/7/6
+ *@Time:11:35
+ *@Description:
+ * */public class Bean {
+}
--- app/src/main/java/bawei/com/lishichang201976/MainActivity.java
+++ app/src/main/java/bawei/com/lishichang201976/MainActivity.java
@@ -2,6 +2,7 @@ package bawei.com.lishichang201976;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
